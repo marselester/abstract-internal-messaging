d133df1ab8ff8f057e71f115f7b123647b3481bd
marselester <marselester@ya.ru>
2013-09-08 22:58:53 +0700

    Add showing of groups on Dashboard page

--- messaging/apps/account/views.py
+++ messaging/apps/account/views.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from django.views import generic
+from django.contrib.auth.models import Group
 
 from messaging.common.decorators import ValidUserMixin
 
@@ -7,3 +8,8 @@ from messaging.common.decorators import ValidUserMixin
 class Dashboard(ValidUserMixin, generic.TemplateView):
 
     template_name = 'account/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Dashboard, self).get_context_data(**kwargs)
+        context['groups'] = Group.objects.all()
+        return context
